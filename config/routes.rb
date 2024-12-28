415f4130477217db3d993769de6a44e33f463271
tranductuan1504 <shenyuyang210@gmail.com>
2024-12-28 09:29:53 +0700

    tuantuan

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  resources :products
+
   get "/products", to: "products#index"
+  post "/products", to: "products#create"
+  get "/products/:id", to: "products#show"
+  get "/blog/:title", to: "blog#show"
+  get "/blog/:slug", to: "blog#show"
+
+  get "/products", to: "products#index"
+
+  get "/products/new", to: "products#new"
+  
+  get "/products/:id", to: "products#show"
+
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+
+  delete "/products/:id", to: "products#destroy"
+
   root "products#index"
 end
